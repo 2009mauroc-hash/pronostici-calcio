e0a0ec514559d26a1654dcdda326e2a05a5eaa83
2009mauroc-hash <2009mauroc@gmail.com>
2026-09-02 23:07:40 +0200

    Update generatore.py

--- generatore.py
+++ generatore.py
@@ -30,7 +30,7 @@ for item in partite:
     """
     
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.7
     )
