1d24a91dc75a354f83e35799bda6dc6d9853a745
2009mauroc-hash <2009mauroc@gmail.com>
2026-09-02 23:26:04 +0200

    Update generatore.py

--- generatore.py
+++ generatore.py
@@ -17,7 +17,7 @@ L'array JSON deve avere esattamente questa struttura:
 """
 
 response = client.chat.completions.create(
-    model="llama3-8b-8192",
+    model="llama-3.1-8b-instant",
     messages=[{"role": "user", "content": prompt}],
     temperature=0.7
 )
